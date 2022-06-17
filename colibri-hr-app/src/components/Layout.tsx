d30fac00e9ee1deb145a9226307e60841b16f941
Andreea Chelaru <andreea@Andreeas-MacBook-Pro.local>
2022-06-17 15:49:08 +0300

    feat(layout): added layout and sidebar components with styling

---
+++ colibri-hr-app/src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "./Sidebar";
+import "./Layout.scss";
+
+const Layout = ({ children }: React.PropsWithChildren) => (
+  <div className="container">
+    <Sidebar />
+    <main>{children}</main>
+  </div>
+);
+
+export default Layout;
---
+++ colibri-hr-app/src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import "./Sidebar.scss";
+
+const Sidebar = () => (
+  <nav aria-label="Sidebar menu" className="sidebar">
+    <h2 className="logo">HR System</h2>
+    <a href="/employees">Employees</a>
+  </nav>
+);
+
+export default Sidebar;

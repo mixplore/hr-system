bcd276638d4cad175a2f6e6699e4365171759dcb
Andreea Chelaru <andreea@Andreeas-MacBook-Pro.local>
2022-06-17 15:52:40 +0300

    feat(layout): added Employees page to show on landing

--- colibri-hr-app/src/App.tsx
+++ colibri-hr-app/src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.scss";
+import Layout from "./components/Layout";
+import Employees from "./pages/Employees";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Employees List
-        </p>
-      </header>
+      <Layout>
+        <Employees />
+      </Layout>
     </div>
   );
 }
---
+++ colibri-hr-app/src/pages/Employees.tsx
@@ -0,0 +1,5 @@
+import "./Employees.scss";
+
+const Employees = () => <h2>Employees List</h2>;
+
+export default Employees;

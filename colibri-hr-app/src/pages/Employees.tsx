f9e6770224c5e237fcd2ce6094cb6e349f016d97
Andreea Chelaru <andreea@Andreeas-MacBook-Pro.local>
2022-06-18 14:33:36 +0300

    feat(employees): add api request and populate table with data

--- colibri-hr-app/src/pages/Employees.tsx
+++ colibri-hr-app/src/pages/Employees.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { getEmployees } from '../api/requests';
 import './Employees.scss';
 
 type Employee = {
@@ -5,50 +7,58 @@ type Employee = {
   first_name: string;
   last_name: string | null;
   email: string | null;
-  date_of_birth: Date;
+  date_of_birth: string;
   industry: string;
   salary: number;
   years_of_experience: number;
 };
 
-type EmployeesPropsType = {
-  data: Employee[];
+const Employees = () => {
+  const [employees, setEmployees] = useState<Employee[]>();
+  useEffect(() => {
+    const getData = async () => {
+      const employeesData = await getEmployees();
+      setEmployees(employeesData);
+    };
+    getData();
+  }, []);
+
+  return (
+    <>
+      <h2>Employees List</h2>
+      {employees && employees.length ? (
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Date of birth</th>
+                <th>Industry</th>
+                <th>Years of Experience</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((employee) => (
+                <tr key={employee.id}>
+                  <td data-label={'ID'}>{employee.id}</td>
+                  <td data-label={'First Name'}> {employee.first_name}</td>
+                  <td data-label={'Last Name'}>{employee.last_name}</td>
+                  <td data-label={'Email'}>{employee.email}</td>
+                  <td data-label={'Date of Birth'}>{employee.date_of_birth}</td>
+                  <td data-label={'Industry'}>{employee.industry}</td>
+                  <td data-label={'Years of Experience'}>{employee.years_of_experience}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="empty-state-text">No data found</div>
+      )}
+    </>
+  );
 };
-
-const Employees = ({ data }: EmployeesPropsType) => (
-  <>
-    <h2>Employees List</h2>
-    {data && data.length ? (
-      <table className="table-container">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Date of birth</th>
-            <th>Industry</th>
-            <th>Years of Experience</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((employee) => (
-            <tr key={employee.id}>
-              <td>{employee.id}</td>
-              <td>{employee.first_name}</td>
-              <td>{employee.last_name}</td>
-              <td>{employee.email}</td>
-              <td>{employee.date_of_birth.toLocaleDateString()}</td>
-              <td>{employee.industry}</td>
-              <td>{employee.years_of_experience}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    ) : (
-      <div className="empty-state-text">No data found</div>
-    )}
-  </>
-);
-
 export default Employees;

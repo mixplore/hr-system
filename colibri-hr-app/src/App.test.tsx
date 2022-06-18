a9d86826b2653254c5d42234ad4d0dc3c802e905
Andreea Chelaru <andreea@Andreeas-MacBook-Pro.local>
2022-06-18 12:58:33 +0300

    chore(cleanup): removed unnecessary code styles and react import, corrected typo

--- colibri-hr-app/src/App.test.tsx
+++ colibri-hr-app/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders title', () => {
   render(<App />);
-  const tittleElement = screen.getByText(/Employees List/i);
-  expect(tittleElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Employees List/i);
+  expect(titleElement).toBeInTheDocument();
 });
